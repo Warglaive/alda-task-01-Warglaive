90a4e0378f59abbc6da4b5473928dd85d903fd39
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-09-05 18:10:04 +0200

    SimpleTimeImpl

--- simpletime/simpletimeimpl/src/main/java/simpletimeimpl/SimpleTimeImpl.java
+++ simpletime/simpletimeimpl/src/main/java/simpletimeimpl/SimpleTimeImpl.java
@@ -10,15 +10,13 @@ public class SimpleTimeImpl implements Time {
     final private int totalTimeInMins;
 
     public SimpleTimeImpl(int hours, int minutes) {
-        if (minutes > 59) {
+        if (hours > 23 || minutes > 59) {
+            throw new IllegalArgumentException("Illegal hours or minutes input");
+        }
+
+        /* if (minutes > 59) {
             throw new IllegalArgumentException("Minutes can NOT be > 59");
-        }
-
-        if (hours > 23) {
-            throw new IllegalArgumentException("Hours can NOT be > 23");
-        }
-
-
+        }*/
         //
         int tempTotalMins = (hours * 60) + minutes;
         if (tempTotalMins < 0) {
