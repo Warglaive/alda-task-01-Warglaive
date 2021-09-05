7caec9689412d9923c6a475f8485ee33292b41c8
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-09-05 15:00:01 +0200

    remove exceptions from duration class

--- simpletime/simpletimeimpl/src/main/java/simpletimeimpl/SimpleTimeImpl.java
+++ simpletime/simpletimeimpl/src/main/java/simpletimeimpl/SimpleTimeImpl.java
@@ -13,7 +13,8 @@ public class SimpleTimeImpl implements Time {
         this.totalTimeInMins = hours * 60 + minutes;
         if (this.totalTimeInMins < 0) {
             throw new IllegalArgumentException("Total minutes can NOT be lower than 0");
-        }  if (this.totalTimeInMins > 24 * 60) {
+        }
+        if (this.totalTimeInMins > 24 * 60) {
             throw new IllegalArgumentException("Total minutes can NOT be bigger than 24 * 60");
         }
     }
